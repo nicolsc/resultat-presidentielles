37063e009381e4ff709df1c0f5d1e615bd03fccf
Nicolas Lesconnec <nicolas.lesconnec.pro@gmail.com>
2012-04-19 19:04:16 +0200

    init project
    Signed-off-by: Nicolas Lesconnec <nicolas.lesconnec.pro@gmail.com>

---
+++ backend/server.js
@@ -0,0 +1,30 @@
+
+var express = require('express'),
+    request = require('request'),
+    fs = require('fs'),
+    requirejs=require('requirejs'),
+    expressApp = express.createServer(),
+    port=process.env.PORT || 40104;
+
+
+testPath = __dirname+'/../public';
+expressApp.use(express.static(testPath));
+expressApp.use(express.bodyParser());
+
+expressApp.get("/",function(req,res,next) {
+  fs.readFile("public/index.html","utf-8",function(err,data) { 
+    res.send(data);
+  });
+});
+});
+
+expressApp.listen(port);
+console.log('Server listening on port', port)
+
+
+var serialize = function(obj) {
+  var str = [];
+  for(var p in obj)
+     str.push(p + "=" + encodeURIComponent(obj[p]));
+  return str.join("&");
+};
---
+++ src/app.js
@@ -0,0 +1,8 @@
+define(['joshlib!vendor/underscore','joshlib!vendor/backbone','joshlib!router',
+        'joshlib!ui/panel','joshlib!ui/list', 'joshlib!ui/verticallist'],
+  function(_,Backbone,Router,
+            Panel, List, VerticalList) {
+
+
+  }
+ );
